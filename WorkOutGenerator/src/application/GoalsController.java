a7cbcdf85c5570fd2606dd477017bf8c2d0cb8cd
Aiden Johnson <ajcool.aj2@gmail.com>
2020-11-28 17:27:33 -0600

    CHANGES

--- WorkOutGenerator/src/application/GoalsController.java
+++ WorkOutGenerator/src/application/GoalsController.java
@@ -7,33 +7,40 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GoalsController {
-	
+	public boolean isLean = false;
+	public boolean isBuff = false;
 	@FXML
 	private AnchorPane mainPane;
 	@FXML
-	private MenuButton LeanOrBuff;
-	@FXML
-	private MenuButton Weight;
-	@FXML
-	private MenuButton Nutritions;
-	@FXML
-	private MenuItem lean;
-	@FXML
-	private MenuItem buff;
+	public TextArea calories;
 	
 	@FXML
-	public void chooseTheType(ActionEvent event) throws IOException{
-		
+	public void Lean(ActionEvent event) throws IOException{
+		isLean = true;
+		isBuff = false;
+	}
+	@FXML
+	public void Buff(ActionEvent event) throws IOException{
+		isBuff = true;
+		isLean = false;
 	}
 	@FXML
 	public void NextBtn(ActionEvent event) throws IOException {
+		if(calories.getText().isEmpty()) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("There arent any calories entered!!!");
+			alert.show();
+		}
 		mainPane = FXMLLoader.load(getClass().getResource("Days.fxml"));
 		Scene scene = new Scene(mainPane);
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
