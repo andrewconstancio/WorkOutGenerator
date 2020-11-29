1c83352620a95e3782b3d10aa1359288ee8431f3
andrewconstancio <andrewconstancio@users.noreply.github.com>
2020-11-29 01:50:29 -0600

    ccc
    cc

--- WorkOutGenerator/src/Model/ProccesingData.java
+++ WorkOutGenerator/src/Model/ProccesingData.java
@@ -1,22 +1,33 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import Model.MainModel;
 
 import application.User;
+import application.UserWorkOutSchedule;
+import application.WorkOut;
 
 public class ProccesingData {
 
 	public static void make_user_schedule() {
 		
+		
+		UserWorkOutSchedule sched = new UserWorkOutSchedule();
+		
+		ArrayList<WorkOut> muscleArray = MainModel.getMuscleWorkouts();
+		ArrayList<WorkOut> cardioArray = MainModel.getCardioWorkouts();
+		
+		WorkOut test = muscleArray.get(1);
+		
+		System.out.println(test.name);
+		
 		int i;
 		for(i = 0; i < 7; ++i) {
 			if(User.days[i] == 1) {
 				if(User.gender == "male") {
-					//
-					
+					sched.mondayWorkouts[0] = (WorkOut) muscleArray.get(3);
 				}
-			} else {
-				
 			}
 		}
 	}
