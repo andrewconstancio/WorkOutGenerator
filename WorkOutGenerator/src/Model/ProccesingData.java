064681655e709cd92d4d476e37b6202eb0acfe67
andrewconstancio <andrewconstancio@users.noreply.github.com>
2020-11-29 01:51:52 -0600

    fd
    fd

--- WorkOutGenerator/src/Model/ProccesingData.java
+++ WorkOutGenerator/src/Model/ProccesingData.java
@@ -1,24 +1,18 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import Model.MainModel;
 
 import application.User;
+import application.UserWorkOutSchedule;
+import application.WorkOut;
 
 public class ProccesingData {
 
 	public static void make_user_schedule() {
 		
-		int i;
-		for(i = 0; i < 7; ++i) {
-			if(User.days[i] == 1) {
-				if(User.gender == "male") {
-					
-					
-				}
-			} else {
-				
-			}
-		}
+	
 	}
 	
 	
