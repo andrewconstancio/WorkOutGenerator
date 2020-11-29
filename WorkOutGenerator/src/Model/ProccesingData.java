96d7f8f4247e0feb2f897553b75dceed936bc992
Aiden Johnson <ajcool.aj2@gmail.com>
2020-11-29 01:52:32 -0600

    Update ProccesingData.java

--- WorkOutGenerator/src/Model/ProccesingData.java
+++ WorkOutGenerator/src/Model/ProccesingData.java
@@ -1,18 +1,21 @@
 package Model;
 
 import java.util.Arrays;
-
-import application.ScheduleController;
 import application.User;
 
 public class ProccesingData {
 
 	public static void make_user_schedule() {
-		
 		int i;
 		for(i = 0; i < 7; ++i) {
 			if(User.days[i] == 1) {
 				if(User.gender == "male") {
+					
+				}
+				else if(User.gender == "female") {
+					
+				}
+				else {
 					
 				}
 			}
