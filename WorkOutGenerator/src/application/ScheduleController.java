726264255f9bc6cef7cd72a1f910327ae6b1ee46
Aiden Johnson <ajcool.aj2@gmail.com>
2020-11-28 18:33:36 -0600

    ScheduleController
    restart button

--- WorkOutGenerator/src/application/ScheduleController.java
+++ WorkOutGenerator/src/application/ScheduleController.java
@@ -1,5 +1,25 @@
 package application;
 
-public class ScheduleController {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ScheduleController {
+	
+	@FXML
+	private AnchorPane mainPane;
+	@FXML
+	public void Restart(ActionEvent event) throws IOException{
+		mainPane = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(mainPane);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
 }
