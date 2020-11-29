56b36a96065dc3681e51b62402882271ce96b415
Aiden Johnson <ajcool.aj2@gmail.com>
2020-11-28 18:07:01 -0600

    interger fixed

--- WorkOutGenerator/src/application/GoalsController.java
+++ WorkOutGenerator/src/application/GoalsController.java
@@ -26,13 +26,31 @@ public class GoalsController {
 	
 	@FXML
 	public void Lean(ActionEvent event) throws IOException{
-		isLean = true;
-		isBuff = false;
+		//isBuff = false;
+		if(isLean == true) {
+			isLean = false;
+		}
+		else {
+			isLean = true;
+		}
 	}
 	@FXML
 	public void Buff(ActionEvent event) throws IOException{
-		isBuff = true;
-		isLean = false;
+		//isLean = false;
+		if(isBuff == true) {
+			isBuff = false;
+		}
+		else {
+			isBuff = true;
+		}
+	}
+	public static boolean isInteger(String str) {
+	    try {
+	        Integer.parseInt(str);
+	        return true;
+	    } catch (NumberFormatException nfe) {
+	        return false;
+	    }
 	}
 	@FXML
 	public void NextBtn(ActionEvent event) throws IOException {
@@ -41,9 +59,19 @@ public class GoalsController {
 			alert.setContentText("There arent any calories entered!!!");
 			alert.show();
 		}
+		else if(isInteger(calories.getText()) == false) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Calories has to be number!!!");
+			alert.show();
+		}
 		else if(isLean == false && isBuff == false) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setContentText("you havent chose the goal of the body type!!!");
+			alert.show();
+		}
+		else if(isLean == true && isBuff == true) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("you can only pick one body type!!!");
 			alert.show();
 		}
 		else {
